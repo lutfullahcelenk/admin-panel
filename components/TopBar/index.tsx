e10175a0f417e9c82b15d0151a013686a0ac65f9
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-20 17:10:59 +0300

    sitebanner is created

--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -1,12 +1,30 @@
 import Image from "next/image";
 import React from "react";
 
-const TopBar = () => {
+type ITopbar = {
+  isOpen: boolean;
+  setIsOpen: any;
+};
+
+const TopBar = ({ isOpen, setIsOpen }: ITopbar) => {
   return (
     <div className="px-2 bg-white py-0.5 h-18 flex">
-      <div className="justify-start hidden w-1/2 p-3 md:flex left gap-x-3">
-        <Image src="/assets/person.png" alt="avatar" width="40" height="40" />
-        <span className="block p-2 font-bold">Lütfullah ÇELENK</span>
+      <div className="justify-start w-1/2 p-3 md:flex left gap-x-3">
+        <div className="hidden md:block">
+          <Image src="/assets/person.png" alt="avatar" width="40" height="40" />
+        </div>
+        <span className="hidden p-2 font-bold md:block">Lütfullah ÇELENK</span>
+        <span
+          className="block p-2 font-bold md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <Image
+            src="/assets/hamburger-menu.ico"
+            alt="avatar"
+            width="40"
+            height="40"
+          />
+        </span>
       </div>
 
       <div className="flex items-center justify-end w-full md:w-1/2 right">
@@ -29,7 +47,9 @@ const TopBar = () => {
             height="28"
           />
         </div>
-        <a className="px-2" href="">Logout</a>
+        <a className="px-2" href="">
+          Logout
+        </a>
       </div>
     </div>
   );
