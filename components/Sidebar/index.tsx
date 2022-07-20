3bd105d3e79903e578b1fc01256318a969eec7a7
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-20 17:10:40 +0300

    hamburger menu functionality is done

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -2,12 +2,27 @@ import Image from "next/image";
 import React from "react";
 import { navbarItems } from "../../constants/navbarItems";
 
-const Sidebar = () => {
+type ISidebar = {
+  isOpen: boolean;
+  setIsOpen: any;
+};
+
+const Sidebar = ({ isOpen, setIsOpen }: ISidebar) => {
   return (
-    <div className="h-screen bg-indigo-800 w-44 md:w-1/3">
-      <div className="bg-indigo-700">
-        <div className="p-5 text-2xl font-bold tracking-wide text-gray-200 uppercase">
+    <div
+      className={`${
+        isOpen ? "block z-10" : "hidden"
+      } h-screen absolute w-full bg-indigo-800 md:block md:w-80 md:relative`}
+    >
+      <div className="flex items-center justify-between p-5 bg-indigo-700">
+        <div className="text-2xl font-bold tracking-wide text-gray-200 uppercase">
           Super Lutfullah
+        </div>
+        <div
+          className={`${isOpen ? "block" : "hidden"} md:hidden p-4 `}
+          onClick={() => setIsOpen(false)}
+        >
+          <Image src="/assets/close.png" alt="cross" width={40} height={40} />
         </div>
       </div>
 
