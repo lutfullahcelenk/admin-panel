7767badf4e364c8e332520cf5d109f455a6a54d9
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-20 17:10:28 +0300

    hamburger menu is added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 //components
 import Sidebar from "../components/Sidebar";
+import SiteBanner from "../components/SiteBanner";
 import TopBar from "../components/TopBar";
 
 const Home: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
 
       <div className="flex-1">
-        <div>
-          <TopBar />
-        </div>
+        <TopBar setIsOpen={setIsOpen} isOpen={isOpen} />
+        <SiteBanner />
       </div>
     </div>
   );
