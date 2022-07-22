5c8166c23d7025c0408d76a0643b12a69ffec328
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-22 23:10:15 +0300

    cards dummy data is formed

---
+++ constants/cards.tsx
@@ -0,0 +1,30 @@
+export const cards = [
+    {
+        id:0,
+        title: "Lutfullah Plus",
+        tag: "Sales",
+        price: "24,780",
+        percentage: "+49",
+    },
+    {
+        id:1,
+        title: "Lutfullah Pro",
+        tag: "Discount",
+        price: "24,780",
+        percentage: "-9",
+    },
+    {
+        id:2,
+        title: "Lutfullah Plus",
+        tag: "Cripto Currency",
+        price: "24,780",
+        percentage: "+13",
+    },
+    {
+        id:3,
+        title: "Lutfullah Pro",
+        tag: "Inflation",
+        price: "24,780",
+        percentage: "-57",
+    },
+]
